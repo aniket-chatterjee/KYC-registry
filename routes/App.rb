6e1b8b23525a4d39eb39ba8a1cff80a27ed0ec44
Neo <neo@localhost.localdomain>
2018-03-12 18:02:06 +0530

    New KYC UI

--- routes/App.rb
+++ routes/App.rb
@@ -1,12 +1,22 @@
 class App<Roda
     route do |r|
-      r.root do
-        r.redirect "kyc-registry"
-      end
       r.on "kyc-registry" do
+          r.on "new" do
+            r.get do
+              view "kyc_form/new"
+            end
+          end
+          r.on "update" do
+            r.get do
+              view "kyc_form/update"
+            end
+          end
           r.get do
             view "kyc_form/index"
           end
+      end
+      r.root do
+        r.redirect "kyc-registry"
       end
     end
 end
